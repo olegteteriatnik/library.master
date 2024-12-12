3366ab3a22d6a2f57b242eee4702716c2643ad4e
olehteteriatnyk <oleh.teteriatnyk@namecheap.com>
2024-12-12 08:02:18 +0100

    feat(8695zj4uh): refactor FileReader service, cover possible errors by input file path

--- src/services/FileReader/FileReader.ts
+++ src/services/FileReader/FileReader.ts
@@ -1,15 +1,11 @@
-import * as fs from 'fs';
+import { promises as fs } from 'fs';
 
 export default class FileReader {
-    public readFile(filePath: string): Promise<string> {
-        return new Promise((resolve, reject) => {
-            fs.readFile(filePath, 'utf8', (err: any, data: any) => {
-                if (err) {
-                    reject(new Error(`Error reading file: ${err.message}`));
-                } else {
-                    resolve(data);
-                }
-            });
-        });
+    public async readFile(filePath: string): Promise<string> {
+        try {
+            return  await fs.readFile(filePath, 'utf8');
+        } catch (error: any) {
+            throw new Error(`Error reading file: ${error.message}`)
+        }
     }
 }
