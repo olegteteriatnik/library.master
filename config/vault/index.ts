e98724890ba40530dd5777572a47e826cd9beebc
olehteteriatnyk <oleh.teteriatnyk@namecheap.com>
2025-01-09 08:14:48 +0100

    feat(8695zj4uh): restructure secrets using API, create Docker file

--- config/vault/index.ts
+++ config/vault/index.ts
@@ -1,25 +1,66 @@
-import { exec } from 'child_process';
-import { promisify } from 'util';
+import axios from 'axios';
+import dotenv from 'dotenv';
 import config from './config';
 
-const execAsync = promisify(exec);
+dotenv.config();
+
+function getEnvVariable(name: string): string {
+    const value = process.env[name];
+    if (!value) {
+        throw new Error(`Environment variable ${name} is missing`);
+    }
+    return value;
+}
 
 export async function getSecret(key: keyof typeof config) {
+    const HCP_CLIENT_ID = getEnvVariable('HCP_CLIENT_ID');
+    const HCP_CLIENT_SECRET = getEnvVariable('HCP_CLIENT_SECRET');
+    const HCP_AUTH_URL = getEnvVariable('HCP_AUTH_URL');
+    const HCP_API_BASE_URL = getEnvVariable('HCP_API_BASE_URL');
+
+    if (!HCP_CLIENT_ID || !HCP_CLIENT_SECRET || !HCP_CLIENT_SECRET) {
+        throw new Error('HCP_CLIENT_ID or HCP_CLIENT_SECRET is missing in environment variables');
+    }
+
     try {
-        const { stdout } =  await execAsync(
-            `hcp vault-secrets secrets open --app LibraryMaster ${config[key]}`
+        const tokenResponse = await axios.post(
+            HCP_AUTH_URL,
+            new URLSearchParams({
+                client_id: HCP_CLIENT_ID,
+                client_secret: HCP_CLIENT_SECRET,
+                grant_type: 'client_credentials',
+                audience: 'https://api.hashicorp.cloud',
+            }).toString(),
+            {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+            }
         );
 
-        const lines = stdout.split('\n');
-        const valueLine = lines.find(line => line.trim().startsWith('Value:'));
+        const HCP_API_TOKEN = tokenResponse.data.access_token;
 
-        if (!valueLine) {
-            throw new Error('Could not find "Value:" in command output');
+        if (!HCP_API_TOKEN) {
+            new Error('Failed to retrieve API token');
         }
 
-        const jsonString = valueLine.replace('Value:', '').trim();
+        const secretResponse = await axios.get(
+            HCP_API_BASE_URL,
+            {
+                headers: {
+                    Authorization: `Bearer ${HCP_API_TOKEN}`,
+                },
+            }
+        );
 
-        return JSON.parse(jsonString);
+        const secretData = secretResponse.data.secrets.find((secret: any) => secret.name === key);
+        const secretValue = JSON.parse(secretData.static_version.value);
+
+        if (!secretData) {
+            new Error(`Secret "${key}" not found`);
+        }
+
+        return secretValue;
     } catch (err: any) {
         console.error(`Error fetching secret "${config[key]}`, err.message);
         throw err;
