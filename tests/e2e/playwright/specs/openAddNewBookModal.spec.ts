f16f733e97dd7298d946305463f0c22e46f5a5aa
olehteteriatnyk <oleh.teteriatnyk@namecheap.com>
2025-04-11 18:20:17 +0200

    test: assign test id to openAddNewBookModal spec

--- tests/e2e/playwright/specs/openAddNewBookModal.spec.ts
+++ tests/e2e/playwright/specs/openAddNewBookModal.spec.ts
@@ -4,7 +4,7 @@ import apiHelper from '../helpers/apiHelper';
 import BooksListPage from '../pages/BooksListPage';
 
 test.describe('Add New Book modal cases.', () => {
-    test('It could be possible to open Add New Book modal.', async ({ page }) => {
+    test('@T2e3a0862 It could be possible to open Add New Book modal.', async ({ page }) => {
         const userData = await commonHelper.getUserData();
         const booksListPage = new BooksListPage(page);
         await apiHelper.signInToLibraryMasterApp(page, userData);
